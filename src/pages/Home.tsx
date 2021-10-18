c6537b5fd6413546f730130348948a480b7fbdcb
thiagoaxsilva <thiagoaxsilva@hotmail.com>
2021-10-17 22:53:52 -0300

    Finished

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,43 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
 
-import { Header } from '../components/Header';
-import { Task, TasksList } from '../components/TasksList';
-import { TodoInput } from '../components/TodoInput';
+import { Header } from "../components/Header";
+import { Task, TasksList } from "../components/TasksList";
+import { TodoInput } from "../components/TodoInput";
 
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      done: false,
+    };
+
+    setTasks((allTasks) => [...allTasks, newTask]);
   }
 
   function handleToggleTaskDone(id: number) {
+    const tasksCopy = tasks;
+    const completedTask = tasksCopy.find((task) => task.id === id);
+    if (completedTask) {
+      completedTask.done = true;
+      tasksCopy.splice(tasksCopy.indexOf(completedTask), 1, completedTask);
+      setTasks([...tasksCopy]);
+    }
     //TODO - toggle task done if exists
   }
 
   function handleRemoveTask(id: number) {
+    const tasksCopy = tasks;
+    const completedTask = tasksCopy.find((task) => task.id === id);
+    console.log(id);
+    if (completedTask) {
+      tasksCopy.splice(tasksCopy.indexOf(completedTask), 1);
+      console.log(tasksCopy);
+      setTasks([...tasksCopy]);
+    }
     //TODO - remove task from state
   }
 
@@ -26,18 +47,18 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EBEBEB'
-  }
-})
+    backgroundColor: "#EBEBEB",
+  },
+});
